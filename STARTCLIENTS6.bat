eb9e9d6dd88ec5bfe20f44fc65a771dac320ff19
Andrew <syberspaz@gmail.com>
2021-06-25 10:26:55 -0400

    Create STARTCLIENTS6.bat
    this is for my 6 camera setup

---
+++ STARTCLIENTS6.bat
@@ -0,0 +1,6 @@
+start bin/LiveScanClient.exe 558 360 0 0 
+start bin/LiveScanClient.exe 558 360 558 0 
+start bin/LiveScanClient.exe 558 360 0 360 
+start bin/LiveScanClient.exe 558 360 558 360 
+start bin/LiveScanClient.exe 558 360 0 720 
+start bin/LiveScanClient.exe 558 360 558 720 
